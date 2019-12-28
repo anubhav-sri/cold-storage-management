c4c0ff471e8f8131b4eb79a3a05c905c4e01fe66
Anubhav Srivastava <anubhav.v.sri@gmail.com>
2019-12-28 22:36:05 +0100

    disable flyway

--- src/main/kotlin/com/saikrishna/wms/Application.kt
+++ src/main/kotlin/com/saikrishna/wms/Application.kt
@@ -1,10 +1,11 @@
 package com.saikrishna.wms
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration
 import org.springframework.boot.autoconfigure.jooq.JooqAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [FlywayAutoConfiguration::class])
 class Application
 
     fun main(args: Array<String>) {
