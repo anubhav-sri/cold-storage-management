533524c60e1c62face24887dde87e9be114b75f5
Anubhav Srivastava <anubhav.v.sri@gmail.com>
2020-04-09 00:25:06 +0200

    Fixes the cors config

--- src/main/kotlin/com/saikrishna/wms/configuration/CorsConfiguration.kt
+++ src/main/kotlin/com/saikrishna/wms/configuration/CorsConfiguration.kt
@@ -1,16 +1,17 @@
-package com.saikrishna.wms.configuration
-
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 
 @Configuration
-@EnableWebMvc
-class CorsConfiguration : WebMvcConfigurer {
-    override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping("/**")
-                .allowedMethods("GET", "POST", "PUT")
+class CorsConfiguration {
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**").allowedOrigins("/**")
+            }
+        }
     }
 }
