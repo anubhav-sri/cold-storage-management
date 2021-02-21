46ea072cbda64dea39445e000adb1fbf287d44a4
Anubhav Srivastava <anubhav.srivastava@sezzle.com>
2021-02-21 16:46:43 +0530

    Limit Result to 1

--- src/main/kotlin/com/saikrishna/wms/repositories/LotRepository.kt
+++ src/main/kotlin/com/saikrishna/wms/repositories/LotRepository.kt
@@ -10,6 +10,6 @@ import java.time.LocalDateTime
 @Repository
 interface LotRepository : CrudRepository<Lot, Int> {
 
-	@Query("select sum(lot.number_of_bags) as totalNumberOfBags, sum(lot.number_of_empty_bags_given) as numberOfEmptyBags from lotdb.lot where date between ?1 and ?2")
+	@Query("select sum(lot.number_of_bags) as totalNumberOfBags, sum(lot.number_of_empty_bags_given) as numberOfEmptyBags from lotdb.lot where date between ?1 and ?2 limit 1")
 	fun findSummaryByDateBetween(start: LocalDateTime, end: LocalDateTime) : Summary?
 }
